4e92c726e66c0af139a9bcc1c6a48f925c099874
Fondit <bastien.verdiervaissiere@gmail.com>
2024-03-28 12:11:24 +0100

    dataset.zip

--- machine_learnibg/image.py
+++ machine_learnibg/image.py
@@ -9,7 +9,6 @@ from tensorflow import keras
 from tensorflow.keras.models import Model
 import tensorflow as tf
 
-# url_test = r'https://github.com/anisayari/Youtube-apprendre-le-deeplearning-avec-tensorflow/blob/master/%234%20-%20CNN/pikachu.png?raw=true'
 url_cat = r'https://github.com/Baverdie/ia/blob/main/machine_learnibg/cat.png?raw=true'
 resp = requests.get(url_cat, stream=True).raw
 image_array_cat = np.asarray(bytearray(resp.read()), dtype="uint8")
@@ -27,3 +26,22 @@ image_dog = cv2.imdecode(image_array_dog, cv2.IMREAD_COLOR)
 plt.axis('off')
 plt.imshow(cv2.cvtColor(image_dog, cv2.COLOR_BGR2RGB))
 plt.show()
+
+
+import pathlib
+import os
+data_dir = tf.keras.utils.get_file(
+    "dataset.zip",
+    "https://github.com/Baverdie/ia/blob/main/machine_learnibg/dataset.zip?raw=true",
+    extract=False)
+
+import zipfile
+with zipfile.ZipFile(data_dir, 'r') as zip_ref:
+    zip_ref.extractall('/content/datasets')
+
+data_dir = pathlib.Path('/content/datasets/dataset')
+print(data_dir)
+print(os.path.abspath(data_dir))
+
+image_count = len(list(data_dir.glob('*/*')))
+print(image_count)
